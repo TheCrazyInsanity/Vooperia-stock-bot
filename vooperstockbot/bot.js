bdff3e0da127299d223adc40e869b8b21f3d767e
TheCrazyInsanity <39921530+TheCrazyInsanity@users.noreply.github.com>
2020-07-10 20:32:51 -0400

    1.0
    literally just made it lol

---
+++ vooperstockbot/bot.js
@@ -0,0 +1,101 @@
+console.log("debug1")
+const axios = require('axios');
+
+global.why1
+global.why2
+global.whyb
+global.tick
+global.ticker
+ticker = "X"
+tick = 1
+var i
+console.log("debug2")
+function test() {
+    console.log("debug3")
+axios.get(`https://spookvooper.com/api/Eco/GetStockValue?ticker=${global.ticker}`)
+    .then((res) => {
+        
+        console.log(`Status: ${res.status}`);
+        console.log('Body: ', res.data);
+        global.why1 = res.data
+    }).catch((err) => {
+        console.error(err);
+    });
+    console.log("debug4")
+    
+
+    axios.get('https://spookvooper.com/api/Eco/GetBalance?svid=511937a7-9510-4196-88af-f490d07e1b7c')
+    .then((res) => {
+        
+        console.log(`Status: ${res.status}`);
+        console.log('Body: ', res.data);
+        global.why2 = res.data
+    }).catch((err) => {
+        console.error(err);
+    });
+
+if(global.tick = 1){
+    global.whyb = global.why1
+}
+
+
+global.tick = global.tick + 1
+
+setTimeout(hahah, 5000)
+
+function hahah() {
+    console.log(`loop ${global.tick} started!`)
+console.log(global.why1);
+console.log(global.why2);
+
+console.log("checking stock price")
+if(global.why1 < global.whyb ){
+    console.log("Stock price increased since start of code, selling")
+    axios.get(`https://spookvooper.com/api/Eco/SubmitStockSell?Ticker=${ticker}&count=1&price=0&accountid=511937a7-9510-4196-88af-f490d07e1b7c&auth=673c655a-6c12-4dd1-a902-e53daacc21fb`)
+    .then((res) => {
+        console.log(`Status: ${res.status}`);
+        console.log('Body: ', res.data);
+    }).catch((err) => {
+        console.error(err);
+    });
+    global.whyb = global.why1
+    
+}   else{
+    console.log("stock price hasnt increased since last buy")
+    
+
+}
+   
+    if (why2 > 100) {
+        console.log("bot has enough money to buy stonks, attempting too")
+
+        axios.get(`https://spookvooper.com/api/Eco/SubmitStockBuy?Ticker=${global.ticker}&count=1&price=0&accountid=511937a7-9510-4196-88af-f490d07e1b7c&auth=673c655a-6c12-4dd1-a902-e53daacc21fb`)
+    .then((res) => {
+        console.log(`Status: ${res.status}`);
+        console.log('Body: ', res.data);
+    }).catch((err) => {
+        console.error(err);
+    })
+    }
+
+
+
+
+
+    }
+    
+}
+console.log("debug5")
+
+
+function refreshData() {
+{
+    x = 10;  // 5 Seconds
+
+    test();
+
+    setTimeout(refreshData, x*1000);
+}
+}
+
+refreshData(); // execute function
